163af6ca8d7f46a9f5c6af88b08574243d0af821
mdwyman <maxdwyman@gmail.com>
2019-01-25 16:54:23 -0600

    Sorted zoom/magnification; added HV controls

--- betaScan.py
+++ betaScan.py
@@ -2,17 +2,20 @@ import time
 from os import path
 from pydm import Display
 from scipy.ndimage.measurements import maximum_position
+from scipy.ndimage.interpolation import zoom
 from qtpy.QtWidgets import QWidget, QGridLayout, QPushButton
 from pydm.widgets.line_edit import PyDMLineEdit
 from pydm.widgets.label import PyDMLabel
 import math
 import epics
+import numpy as np
 
 class BetaScan(Display):
 
 	def __init__(self, parent=None, args=None):
 		super(BetaScan, self).__init__(parent=parent, args=args)
 		self.ui.imageView.mousePressEvent = self.get_coord
+		self.ui.imageView.process_image = self.process_image
 
 		self.motorRead = self.ui.EmbeddedMotor.findChild(PyDMLabel,"motorPosRead")
 		self.motorSet = self.ui.EmbeddedMotor.findChild(PyDMLineEdit,"motorPosSet")
@@ -20,11 +23,15 @@ class BetaScan(Display):
 		self.motorGo = self.ui.EmbeddedMotor.findChild(QPushButton,"motorMove")
 		self.motorGo.mousePressEvent = self.move_motor
 		
+		self.imageXSize = epics.PV('PelmeniNDSA:cam1:ArraySize0_RBV')
 		self.imageYSize = epics.PV('PelmeniNDSA:cam1:ArraySize1_RBV')
+		self.binYSize = epics.PV('PelmeniDet:ROI1:BinY_RBV')
+		self.zoomedSize = 1
+		self.imageHeight = 672.0
 		
 		
 	def ui_filename(self):
-		# Point to our UI file
+		# Point to UI file
 		return 'betaScan.ui'
 
 	def ui_filepath(self):
@@ -34,15 +41,29 @@ class BetaScan(Display):
 	def move_motor(self, event):
 		self.motorSet.send_value()
 
+	def zoomed_image(self, new_image, ys, mag):
+		new_image = np.asarray(new_image)
+		slices = [slice(0, new_image.shape[0], 1/mag)]
+		idxs = (np.mgrid[slices]).astype('i')
+		return new_image[tuple(idxs)]
+		
+	def process_image(self, new_image):
+		yIters, xLength = new_image.shape
+		binSize = self.binYSize.get()
+		magnification = math.floor(self.imageHeight/float(yIters))
+		print(self.imageHeight, xLength, yIters, magnification)
+		
+		newer_image = self.zoomed_image(new_image, yIters, magnification)
+		
+		self.zoomedSize = newer_image.shape[0]
+		print("Image size: {}".format(newer_image.shape))
+		return newer_image
+
 	def get_coord(self, event):
         # Check that mouse-click is in image
 		self.coords = [event.pos().x(), event.pos().y()]
 		mouseY = float(event.pos().y())
-		# Need data from ca://PelmeniNDSA:cam1:ArraySize1_RBV since ImageYSize
-		# PyDMLabel has been removed:
-		#yExtent = float(self.ui.ImageYSize.text())
-		yExtent = self.imageYSize.get()
-
+		yExtent = self.zoomedSize
 
 		# Image boundaries returned as [[xmin, xmax], [ymin, ymax]]
 		view = self.ui.imageView.getView()
@@ -59,8 +80,10 @@ class BetaScan(Display):
 					
 		motorCurrPos = float(self.motorRead.text())
 		
-		mouseExtent = 480 ## would prefer to get this number programmatically
 		
+#		mouseExtent = 480 ## would prefer to get this number programmatically
+		mouseExtent = self.ui.imageView.height()
+
 		chosenY = ((1.0 - (mouseY / mouseExtent * (yMax - 
 					yMin) + yMin) / yExtent) * (motorScanStartPos - 
 					(motorScanStopPos + motorScanStepSize)) + motorScanStopPos +
@@ -71,11 +94,9 @@ class BetaScan(Display):
 				steps = (math.floor((chosenY - motorScanStartPos)/motorScanStepSize))
 				chosenY_disc = steps * motorScanStepSize + motorScanStartPos
 				self.motorSet.setText(str(chosenY_disc))
-		
-		mouse_txt = "Mouse click at:"
-		mouse_txt += " ({}, {})".format(self.coords[0], self.coords[1])
-		mouse_txt += "\n Image vertical extent (y_min, y_max): \n"
-		mouse_txt += "({}, {})".format(yMin, yMax)
-
-		yPos_txt = "Mouse click converted to y-position of (clicked value, actual position): \n"
-		yPos_txt += " {}, {}".format(chosenY, motorCurrPos)
+			
+		print("****************************************************************")
+		print("mouseY, mouseExtent: {}, {}".format(mouseY, mouseExtent))
+		print("yMin, yMax, yExtent: {}, {}, {}".format(yMin, yMax, yExtent))
+		print("motor scan start, stop, step: {}, {}, {}".format(motorScanStartPos, motorScanStopPos, motorScanStepSize))
+		print("chosenY, limits: {}, {}".format(chosenY, limits))
